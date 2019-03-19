bab3d49795120ff1b2748dceb1299ad7dcb10cf6
tunc gulec <tuncgulec@gmail.com>
2019-03-19 11:40:48 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace DemoLDAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtDomainAddress = new System.Windows.Forms.TextBox();
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LblDisplayName = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LblStatus = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.LstUsers = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TxtDomainAddress
+            // 
+            this.TxtDomainAddress.Location = new System.Drawing.Point(100, 12);
+            this.TxtDomainAddress.Name = "TxtDomainAddress";
+            this.TxtDomainAddress.Size = new System.Drawing.Size(204, 20);
+            this.TxtDomainAddress.TabIndex = 0;
+            // 
+            // TxtUsername
+            // 
+            this.TxtUsername.Location = new System.Drawing.Point(100, 38);
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(100, 20);
+            this.TxtUsername.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(100, 64);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(100, 20);
+            this.TxtPassword.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LblStatus);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.LblDisplayName);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(7, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(403, 73);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Simple Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Domain Address :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Login Status :";
+            // 
+            // LblDisplayName
+            // 
+            this.LblDisplayName.AutoSize = true;
+            this.LblDisplayName.Location = new System.Drawing.Point(90, 40);
+            this.LblDisplayName.Name = "LblDisplayName";
+            this.LblDisplayName.Size = new System.Drawing.Size(16, 13);
+            this.LblDisplayName.TabIndex = 8;
+            this.LblDisplayName.Text = "!!!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Display Name:";
+            // 
+            // LblStatus
+            // 
+            this.LblStatus.AutoSize = true;
+            this.LblStatus.Location = new System.Drawing.Point(93, 16);
+            this.LblStatus.Name = "LblStatus";
+            this.LblStatus.Size = new System.Drawing.Size(16, 13);
+            this.LblStatus.TabIndex = 10;
+            this.LblStatus.Text = "!!!";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.LstUsers);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(7, 169);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(781, 238);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Get Users";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(677, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 41);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Get Users";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LstUsers
+            // 
+            this.LstUsers.FormattingEnabled = true;
+            this.LstUsers.Location = new System.Drawing.Point(9, 20);
+            this.LstUsers.Name = "LstUsers";
+            this.LstUsers.Size = new System.Drawing.Size(662, 212);
+            this.LstUsers.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TxtDomainAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtDomainAddress;
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblDisplayName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LblStatus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox LstUsers;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TLDAPComp;
+
+namespace DemoLDAP
+{
+    public partial class Form1 : Form
+    {
+        TLDAP LDAP;
+        public Form1()
+        {
+            InitializeComponent();
+            LDAP = new TLDAP();
+            LDAP.OnLogin += LDAP_OnLogin;
+            LDAP.OnGetUsers += LDAP_OnGetUsers;
+        }
+
+        private void LDAP_OnGetUsers(object sender, List<TLdapUser> domainUsers, string message)
+        {
+            if (domainUsers!=null)
+            {
+                if (domainUsers.Count>0)
+                {
+                    foreach (var user in domainUsers)
+                    {
+                        LstUsers.Items.Add("Username: "+user.UserName + " name: " + user.name + " Display: " + user.DisplayName + " desc: " + user.description);
+                    }
+                }
+            }
+        }
+
+        private void LDAP_OnLogin(object sender, TDomainUser domainUser, string message)
+        {
+            if (domainUser!=null)
+            {
+                LblStatus.Text = "Success";                
+                LblDisplayName.Text = domainUser.DisplayName;
+                MessageBox.Show(domainUser.GiveName);
+            }
+            else
+                LblStatus.Text = "Error " + message;                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            LDAP.DomainName = TxtDomainAddress.Text;
+            LDAP.Login(TxtUsername.Text, TxtPassword.Text);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LDAP.DomainName = TxtDomainAddress.Text;
+            LDAP.DomainUserName = TxtUsername.Text;
+            LDAP.DomainUserPassword = TxtPassword.Text;
+            LstUsers.Items.Clear();
+            LDAP.GetUsers();
+        }
+    }
+}
